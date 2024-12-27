b6bbbbf64898a0461523e1bfa478652271ad841c
Noor Rahmi <95263271+rahmi1016@users.noreply.github.com>
2024-12-27 10:20:47 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,73 @@
+# Frontend Mentor - Age calculator app solution
+
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The year is in the future
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: See the age numbers animate to their final number when the form is submitted
+
+### Screenshot
+
+![desktop](./design/desktop.png)
+![mobile](./design/mobile.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+
+### What I learned
+
+Through this project, I get more understanding about z-index application.
+
+### Continued development
+
+After I understand how to upload react project to github, I will focus using reacts and tailwinds
+
+### Useful resources
+
+- [Age Calculator using JavaScript](https://github.com/sujitmahapatra/Age-Calculator-using-JS/tree/main) - This helped me for understanding using javascript to calculate age with current day.
+
+## Author
+
+- Frontend Mentor - [@rahmi1016](https://www.frontendmentor.io/profile/rahmi1016)
+
+## Acknowledgments
+
+- [sujitmahapatra](https://github.com/sujitmahapatra)
---
+++ script.js
@@ -0,0 +1,240 @@
+let resultYears = document.getElementById("result-years");
+let resultMonths = document.getElementById("result-months");
+let resultDays = document.getElementById("result-days");
+
+function warningStyle() {
+  document.getElementById("day-text").style.color = "hsl(0, 100%, 67%)";
+  document.getElementById("month-text").style.color = "hsl(0, 100%, 67%)";
+  document.getElementById("year-text").style.color = "hsl(0, 100%, 67%)";
+  document.getElementById("day").style.border = "0.5px solid hsl(0, 100%, 67%)";
+  document.getElementById("month").style.border = "0.5px solid hsl(0, 100%, 67%)";
+  document.getElementById("year").style.border = "0.5px solid hsl(0, 100%, 67%)";
+}
+
+function redBorder() {
+  document.getElementById("day").style.border = "0.5px solid hsl(0, 100%, 67%)";
+  document.getElementById("month").style.border = "0.5px solid hsl(0, 100%, 67%)";
+  document.getElementById("year").style.border = "0.5px solid hsl(0, 100%, 67%)";
+}
+
+function restartResult() {
+  resultDays.innerText = "--";
+  resultMonths.innerText = "--";
+  resultYears.innerText = "--";
+}
+
+function restartWarning() {
+  dayWarning.innerText = "";
+  monthWarning.innerText = "";
+  yearWarning.innerText = "";
+}
+
+function restartStyle() {
+  document.getElementById("day-text").style.color = "hsl(0, 1%, 44%)";
+  document.getElementById("month-text").style.color = "hsl(0, 1%, 44%)";
+  document.getElementById("year-text").style.color = "hsl(0, 1%, 44%)";
+  document.getElementById("day").style.border = "0.5px solid hsl(0, 1%, 44%)";
+  document.getElementById("month").style.border = "0.5px solid hsl(0, 1%, 44%)";
+  document.getElementById("year").style.border = "0.5px solid hsl(0, 1%, 44%)";
+}
+
+function dayValidate() {
+  let day = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
+  let year = document.getElementById("year").value;
+  let dayWarning = document.getElementById("day-warning");
+  let monthWarning = document.getElementById("month-warning");
+  let yearWarning = document.getElementById("year-warning");
+  let daysInMonth = new Date(year, month, 0).getDate();
+  if (day !== "" && month == "" && year == "") {
+    warningStyle();
+    restartResult();
+    monthWarning.innerText = "This field is required";
+    yearWarning.innerText = "This field is required";
+    if (day < 0 || day > 31) {
+      dayWarning.innerText = "Must be a valid date";
+    } else {
+      dayWarning.innerText = "";
+    }
+  } else if (day !== "" && month !== "" && year == "") {
+    warningStyle();
+    restartResult();
+    dayWarning.innerText = "";
+    monthWarning.innerText = "";
+    yearWarning.innerText = "This field is required";
+  } else if (day !== "" && month !== "" && year !== "") {
+    if (day > daysInMonth) {
+      warningStyle();
+      monthWarning.innerText = "";
+      yearWarning.innerText = "";
+      dayWarning.innerText = "Must be a valid date";
+    } else {
+      restartStyle();
+      monthWarning.innerText = "";
+      yearWarning.innerText = "";
+      dayWarning.innerText = "";
+    }
+  }
+}
+function monthValidate() {
+  let day = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
+  let year = document.getElementById("year").value;
+  let dayWarning = document.getElementById("day-warning");
+  let monthWarning = document.getElementById("month-warning");
+  let yearWarning = document.getElementById("year-warning");
+  let daysInMonth = new Date(year, month, 0).getDate();
+
+  if (day == "" && month !== "" && year == "") {
+    warningStyle();
+    restartResult();
+    if (month < 0 || month > 12) {
+      dayWarning.innerText = "This field is required";
+      yearWarning.innerText = "This field is required";
+      monthWarning.innerText = "Must be a valid month";
+    } else {
+      dayWarning.innerText = "This field is required";
+      yearWarning.innerText = "This field is required";
+      monthWarning.innerText = "";
+    }
+  } else if (day !== "" && month !== "" && year == "") {
+    warningStyle();
+    restartResult();
+    if (month < 0 || month > 12) {
+      dayWarning.innerText = "";
+      yearWarning.innerText = "This field is required";
+      monthWarning.innerText = "Must be a valid month";
+    } else {
+      dayWarning.innerText = "";
+      yearWarning.innerText = "This field is required";
+      monthWarning.innerText = "";
+    }
+  } else if (day !== "" && month !== "" && year !== "") {
+    if (day > daysInMonth) {
+      warningStyle();
+      dayWarning.innerText = "Must be a valid date";
+      monthWarning.innerText = "";
+      yearWarning.innerText = "";
+    } else {
+      restartStyle();
+      dayWarning.innerText = "";
+      monthWarning.innerText = "";
+      yearWarning.innerText = "";
+    }
+  }
+}
+function yearValidate() {
+  let day = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
+  let year = document.getElementById("year").value;
+  let dayWarning = document.getElementById("day-warning");
+  let monthWarning = document.getElementById("month-warning");
+  let yearWarning = document.getElementById("year-warning");
+  let today = new Date();
+  let currentYear = today.getFullYear();
+  let daysInMonth = new Date(year, month, 0).getDate();
+  if (day == "" && month == "" && year !== "") {
+    warningStyle();
+    restartResult();
+    if (year > currentYear) {
+      dayWarning.innerText = "This field is required";
+      monthWarning.innerText = "This field is required";
+      yearWarning.innerText = "Must be in the past";
+    } else if (year < 0) {
+      dayWarning.innerText = "This field is required";
+      monthWarning.innerText = "This field is required";
+      yearWarning.innerText = "Must be a valid year";
+    } else {
+      dayWarning.innerText = "This field is required";
+      monthWarning.innerText = "This field is required";
+      yearWarning.innerText = "";
+    }
+  } else if (day == "" && month !== "" && year !== "") {
+    if (month < 0 || month > 12) {
+      dayWarning.innerText = "This field is required";
+      monthWarning.innerText = "Must be a valid month";
+      yearWarning.innerText = "";
+      console.log("satu");
+    } else {
+      dayWarning.innerText = "This field is required";
+      monthWarning.innerText = "";
+      yearWarning.innerText = "";
+      console.log("dua");
+    }
+  } else if (day !== "" && month !== "" && year !== "") {
+    if (day > daysInMonth) {
+      warningStyle();
+      dayWarning.innerText = "Must be a valid date";
+      monthWarning.innerText = "";
+      yearWarning.innerText = "";
+    } else if (year > currentYear) {
+      dayWarning.innerText = "";
+      monthWarning.innerText = "";
+      yearWarning.innerText = "Must be in the past";
+    } else {
+      restartStyle();
+      dayWarning.innerText = "";
+      monthWarning.innerText = "";
+      yearWarning.innerText = "";
+    }
+  }
+}
+
+function getCalculate() {
+  let day = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
+  let year = document.getElementById("year").value;
+
+  let today = new Date();
+  let currentDay = today.getDate();
+  let currentMonth = today.getMonth() + 1;
+  let currentYear = today.getFullYear();
+  let daysInMonth = new Date(year, month, 0).getDate();
+  let dayWarning = document.getElementById("day-warning");
+  let monthWarning = document.getElementById("month-warning");
+  let yearWarning = document.getElementById("year-warning");
+
+  let restYear, restMonth, restDay;
+  restYear = currentYear - year;
+
+  if (dayWarning.innerText !== "" || monthWarning.innerText !== "" || yearWarning.innerText !== "") {
+    warningStyle();
+    resultDays.innerText = "--";
+    resultMonths.innerText = "--";
+    resultYears.innerText = "--";
+  } else {
+    if (currentMonth >= month) {
+      restMonth = currentMonth - month;
+      resultDays.innerText = restDay;
+      resultMonths.innerText = restMonth;
+      restYear = currentYear - year;
+      resultYears.innerText = restYear;
+    } else {
+      restYear - 1;
+      restMonth = 12 + currentMonth - month;
+      resultDays.innerText = restDay;
+      resultMonths.innerText = restMonth;
+      restYear = currentYear - year;
+      resultYears.innerText = restYear;
+    }
+    if (currentDay >= day) {
+      restDay = currentDay - day;
+      resultDays.innerText = restDay;
+      resultMonths.innerText = restMonth;
+      resultYears.innerText = restYear;
+    } else {
+      restMonth - 1;
+      restDay = daysInMonth + currentDay - day;
+      resultDays.innerText = restDay;
+      resultMonths.innerText = restMonth;
+      resultYears.innerText = restYear;
+    }
+    if (currentMonth < 0) {
+      restMonth = 11;
+      restYear - 1;
+      resultDays.innerText = restDay;
+      resultMonths.innerText = restMonth;
+      resultYears.innerText = restYear;
+    }
+  }
+}
